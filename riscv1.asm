b80d4c572f94b4dcfb0daaf614fff47f4588fef8
Shuii8 <159457684+Shuii8@users.noreply.github.com>
2025-01-30 11:40:39 -0600

    Update riscv1.asm

--- riscv1.asm
+++ riscv1.asm
@@ -1,29 +1,8 @@
  #All rights reserved
-#Copyright belongs to Ernesto Rivera
-#You can use this code freely in your project(s) as long as credit is given :)
+#Copyright belongs to Saul Ramirez
+#You can use this code freely as long as credit is given
 
-#Inspiration taken from a MIPS assembly version done by: https://github.com/AndrewHamm/MIPS-Pong for the 
-#MARS emulator
 
-#The official repository of the RARS emulator can be found in: https://github.com/TheThirdOne/rars
-
-# To run the project:
-# 1) In the upper bar go to Run->Assemble (f3)
-# 2) In the upper bar go to Tools->Bitmap Display
-# 3) Configure the following settings in in the Bitmap Display:
-	# a) Unit Width: 8
-	# b) Unit Height: 8
-	# c) Display Width: 512
-	# d) Display Height: 512
-	# e) Base Address: gp
-	# f) Press connect to program 
-# 4) In the upper bar go to Tools->Keyboard and Display MMIO Simulator and press connect to MIPS
-# 5) In the upper bar go to Run->Go (f5)
-# 6) Click on the lower window of the Keyboard and Display simulator to produce inputs
-
-#Player movement is w and s for the left player and o and l for the right player.
-
-#FOR THE STUDENTS: Internal labels of a function starts with a .
 
 # Here I define the constants that will be used along the code
 .eqv TOTAL_PIXELS, 16384#8192 The total ammount of pixels in the screen
